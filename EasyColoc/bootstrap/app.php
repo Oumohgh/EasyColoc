f97bbff4ba83c63df7377545addf8483b3012dba
oughlane <mohammedoughlane72@gmail.com>
2026-02-27 10:55:08 +0000

    register middlewares :isadmin et banned dans app.php

--- EasyColoc/bootstrap/app.php
+++ EasyColoc/bootstrap/app.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use App\Http\Middleware\CheckBanned;
+use App\Http\Middleware\IsAdmin;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,9 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         $middleware->alias([
+            'check.banned' => CheckBanned::class,
+            'is.admin' => IsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-    
+
